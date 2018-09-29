f51cd20c1d6e7199b6578dd7767382ec605021c3
Harsh Saini <vinaysaini97@gmail.com>
2018-09-29 19:53:26 +0530

    All Bugs in Connection Activity is Fixed Internet Connectivity and Socket TimeOut Exception catches.and show with Dialog Box

--- app/src/main/java/com/example/harshsaini/remotify/MainActivity.java
+++ app/src/main/java/com/example/harshsaini/remotify/MainActivity.java
@@ -1,13 +1,36 @@
 package com.example.harshsaini.remotify;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
+    DrawerLayout drawerLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main1);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        drawerLayout = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigationbar_open,
+                R.string.navigationbar_close);
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+
+        }
     }
 }
+
